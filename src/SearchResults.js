c8eb5302d04e78b1ad87bf7b97ec6a40eebf4b01
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2022-03-24 20:45:39 +0000

    Implement CHT-07

---
+++ src/SearchResults.js
@@ -0,0 +1,43 @@
+import React from "react";
+import moment from "moment";
+
+const SearchResults = props => {
+  //let bookings = JSON.parse("./data/fakeBookings.json")
+  return (
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Title</th>
+        <th>First Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+        <th>Room Id</th>
+        <th>Check-in Date</th>
+        <th>Check-out Date</th>
+        <th>Night</th>
+      </tr>
+      {props.results.map(booking => {
+        return (
+          <tr>
+            <td>{booking.id}</td>
+            <td>{booking.title}</td>
+            <td>{booking.firstName}</td>
+            <td>{booking.surname}</td>
+            <td>{booking.email}</td>
+            <td>{booking.roomId}</td>
+            <td>{booking.checkInDate}</td>
+            <td>{booking.checkOutDate}</td>
+            <td>
+              {moment(booking.checkOutDate).diff(
+                moment(booking.checkInDate),
+                "day"
+              )}
+            </td>
+          </tr>
+        );
+      })}
+    </table>
+  );
+};
+
+export { SearchResults };
