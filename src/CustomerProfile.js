1d52226cecf7baed81c6f197dc0fd70d8e0c5022
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2022-04-03 20:18:04 +0100

    CHT-21 (#48)
    * CHT-21

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CustomerProfile = props => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then(data => {
+        setProfile(data);
+      });
+  }, [props.id]);
+
   return (
     <div>
-      <h1>Customer {props.id} Profile</h1>
-      Customer Profile details goes here
+      {profile && (
+        <div>
+          <h1>Customer {props.id} Profile</h1>
+          Customer Profile details goes here
+          <ul>
+            <li>Email: {profile.email}</li>
+            <li>Phone No: {profile.phoneNumber}</li>
+            <li>VIP: {profile.vip + ""}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
